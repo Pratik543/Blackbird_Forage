9682698e09b3589994e54b88aa5a2f45570f4a3d
Pratik <guptapratik304@gmail.com>
2023-09-09 21:36:18 +0530

    updated logo and link

--- task-1/src/App.js
+++ task-1/src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img
+            src="https://www.propelleraero.com/wp-content/uploads/2023/05/logo.svg"
+            className="App-logo"
+            alt="logo"
+            width="500"
+            height="300"
+          />
+          <p>Click here to learn more about DirtMate</p>
         </a>
       </header>
     </div>
